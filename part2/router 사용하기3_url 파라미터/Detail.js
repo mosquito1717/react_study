35b6d937f00a1513580b8f096d80086c3875a1df
mosquito1717 <80042219+mosquito1717@users.noreply.github.com>
2022-08-16 21:57:50 +0900

    Update Detail.js

--- part2/router 사용하기3_url 파라미터/Detail.js
+++ part2/router 사용하기3_url 파라미터/Detail.js
@@ -2,17 +2,19 @@ import { useParams } from "react-router-dom";
 
 function Detail(props){
 
-    let id = useParams();
+    let {id} = useParams();
 
     let 찾은상품 = props.shoes.find(function(x){
         return x.id == id
     });
+    
+    console.log(typeof(찾은상품.id));
 
     return(
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <img src={'https://codingapple1.github.io/shop/shoes'+(찾은상품.id+1)+'.jpg'} width="100%" />
           </div>
           <div className="col-md-6">
             <h4 className="pt-5">{찾은상품.title}</h4>
